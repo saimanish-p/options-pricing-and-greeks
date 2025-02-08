f2c66e7f6b9b076ae9814f122b52fef14f8a280d
Saimanish-P <161119631+saimanish-p@users.noreply.github.com>
2025-02-08 21:07:56 +0530

    Added all visualisations and styling functions as packages for export

---
+++ src/visualisations/__init__.py
@@ -0,0 +1,33 @@
+# Importing functions from general_plots
+from .general_plots import (
+    plot_price_comparison,
+    plot_volatility_sensitivity,
+    plot_time_to_expiration_sensitivity,
+    plot_strike_price_sensitivity,
+    animate_monte_carlo_simulation,
+    plot_histogram_of_simulated_prices
+)
+
+# Importing functions from greeks_plots
+from .greeks_plots import (
+    plot_first_order_greek,
+    plot_second_order_greek,
+    create_volatility_surface,
+)
+
+# Importing styling functions
+from .styling import render_header
+
+# Optionally, you can define the __all__ variable to specify what is exported
+__all__ = [
+    'plot_price_comparison',
+    'plot_volatility_sensitivity',
+    'plot_time_to_expiration_sensitivity',
+    'plot_strike_price_sensitivity',
+    'animate_monte_carlo_simulation',
+    'plot_histogram_of_simulated_prices',
+    'plot_first_order_greek',
+    'plot_second_order_greek',
+    'create_volatility_surface',
+    'render_header'
+]
